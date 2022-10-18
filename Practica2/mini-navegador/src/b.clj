174146481e6a9eb681d413289c14cf0bd012f028
Saúl Hernández <47616009+saulhernandez99@users.noreply.github.com>
2022-10-18 00:02:44 -0500

    b

--- Practica2/mini-navegador/src/b.clj
+++ Practica2/mini-navegador/src/b.clj
@@ -1,4 +1,18 @@
 (ns b)
 
-(defn b [string]
-  (str string "Respondido"))
+(defn b [peticion]
+  (let [s   (Socket. "www.columbia.edu" 80)
+        wtr (PrintWriter. (.getOutputStream s))]
+    (.println wtr peticion)
+    (.println wtr "Host: www.columbia.edu")
+    (.println wtr "")
+    (.flush wtr)
+    ;;
+    (let [rdr (-> s
+                  (.getInputStream)
+                  (InputStreamReader.)
+                  (BufferedReader.))
+          a   (atom nil)]
+
+      (while (some? (reset! a (.readLine rdr)))
+        (println @a)))))
