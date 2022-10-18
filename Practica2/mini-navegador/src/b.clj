4a0b1d519a89a3c64ce4ce88d8166cbd75a8455e
Saúl Hernández <47616009+saulhernandez99@users.noreply.github.com>
2022-10-18 00:10:48 -0500

    b

--- Practica2/mini-navegador/src/b.clj
+++ Practica2/mini-navegador/src/b.clj
@@ -1,18 +1,22 @@
 (ns b)
 
 (defn b [peticion]
-  (let [s   (Socket. "www.columbia.edu" 80)
-        wtr (PrintWriter. (.getOutputStream s))]
-    (.println wtr peticion)
-    (.println wtr "Host: www.columbia.edu")
-    (.println wtr "")
-    (.flush wtr)
-    ;;
-    (let [rdr (-> s
-                  (.getInputStream)
-                  (InputStreamReader.)
-                  (BufferedReader.))
-          a   (atom nil)]
+  (try
+    (let [s   (Socket. "www.columbia.edu" 80)
+          wtr (PrintWriter. (.getOutputStream s))]
+      (.println wtr peticion)
+      (.println wtr "Host: www.columbia.edu")
+      (.println wtr "")
+      (.flush wtr)
+      ;;
+      (let [rdr (-> s
+                    (.getInputStream)
+                    (InputStreamReader.)
+                    (BufferedReader.))
+            a   (atom nil)]
 
-      (while (some? (reset! a (.readLine rdr)))
-        (println @a)))))
+        (while (some? (reset! a (.readLine rdr)))
+          (println @a))))
+    
+    (catch Exception e
+      (prn e))))
